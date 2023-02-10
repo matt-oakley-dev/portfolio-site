df2236b01e9f3ec0f82d2d1a617dd218c1863e71
matt-oakley-dev <matt.oakley@wearesmile.com>
2023-02-10 15:07:08 +0000

    Make the icon menu UX more obvious

--- src/compontents/icons/technologyRow.js
+++ src/compontents/icons/technologyRow.js
@@ -12,27 +12,27 @@ import ExperienceRow from "./experienceRow";
 import '../../scss/icons.scss';
 
 export default function TechnologyRow() {
-    const [selectedTechnology, setSelectedTechnology] = useState('');
+    const [selectedTechnology, setSelectedTechnology] = useState('php');
 
-    const handleTechnologySelect = (technologyKey) => {
-        if ( technologyKey === selectedTechnology ) {
-            technologyKey = false;
+    const handleTechnologySelect = (e, technologyKey) => {
+        var cusid_ele = document.getElementsByClassName('highlight-border-bottom');
+        for (var i = 0; i < cusid_ele.length; ++i) {
+            document.getElementById(cusid_ele[i].id).classList.remove('highlight-border-bottom')
         }
 
-        console.log(technologyKey);
-
+        document.getElementById(e.target.id).classList.add('highlight-border-bottom')
         setSelectedTechnology(technologyKey);
     }
 
     return (
         <>
             <div className='social-media-row justify-content-left'>
-                <img onClick={() => handleTechnologySelect('php')} src={php} className="tech-media-icon" alt="php logo"/>
-                <img onClick={() => handleTechnologySelect('aws')} src={aws} className="tech-media-icon"  alt="aws logo"/>
-                <img onClick={() => handleTechnologySelect('js')} src={javascript} className="tech-media-icon" alt="javascript logo"/>
+                <img id="php-id" onClick={(e) => handleTechnologySelect(e, 'php')} src={php} className="tech-media-icon highlight-border-bottom" alt="php logo"/>
+                <img id="aws-id" onClick={(e) => handleTechnologySelect(e, 'aws')} src={aws} className="tech-media-icon"  alt="aws logo"/>
+                <img id="js-id" onClick={(e) => handleTechnologySelect(e, 'js')} src={javascript} className="tech-media-icon" alt="javascript logo"/>
                 {/* <img onClick={() => handleTechnologySelect('database')} src={database} className="tech-media-icon" alt="database logo"/> */}
-                <img onClick={() => handleTechnologySelect('wordpress')} src={wordpress} className="tech-media-icon" alt="wordpress logo"/>
-                <img onClick={() => handleTechnologySelect('git')} src={git} className="tech-media-icon" alt="git logo"/>
+                <img id="wordpress-id" onClick={(e) => handleTechnologySelect(e, 'wordpress')} src={wordpress} className="tech-media-icon" alt="wordpress logo"/>
+                <img id="git-id" onClick={(e) => handleTechnologySelect(e, 'git')} src={git} className="tech-media-icon" alt="git logo"/>
             </div>
 
             {selectedTechnology && (
