f30a538ccb95ffa48f699dca66d3b0b0cffdb622
Matt Oakley <matt.oakley@wearesmile.com>
2023-03-31 21:18:02 +0100

    chatgpt response

--- src/lib/generateChatGPTResponse.js
+++ src/lib/generateChatGPTResponse.js
@@ -1,5 +1,5 @@
 const API_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
-const YOUR_API_KEY = '';
+const YOUR_API_KEY = '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';
 
 const generateChatGPTResponse = async (prompts) => {
 	const response = await fetch(API_ENDPOINT, {
