04b7da8390b608490f9de0cbce881660251c447d
Matt Oakley <matt.oakley@wearesmile.com>
2023-04-03 10:30:01 +0100

    chatgpt updates

--- src/compontents/chatgpt/ThreadResponse.js
+++ src/compontents/chatgpt/ThreadResponse.js
@@ -1,14 +1,24 @@
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import openai from '../../images/chatgpt/openai.svg';
+import heart from '../../images/chatgpt/heart.svg';
 
 export default function ThreadResponse({content}) {
+	const saveTofavourites = (event) => {
+		console.log('save to favourites');
+	}
+
 	return (
 		<div className="chatGPTResponse">
-			<img src={openai} className="chatgpt-thread-icon"  alt="chatgpt logo"/>
-			<div className="chatGPTContent">
-				<ReactMarkdown>
-					{content}
-				</ReactMarkdown>
+			<div className="content-parent">
+				<img src={openai} className="chatgpt-thread-icon"  alt="chatgpt logo"/>
+				<div className="chatGPTContent">
+					<ReactMarkdown>
+						{content}
+					</ReactMarkdown>
+				</div>
+			</div>
+			<div>
+				<img src={heart} className="heart-thread-icon" onClick={saveTofavourites} alt="chatgpt logo"/>
 			</div>
 		</div>
   	)
